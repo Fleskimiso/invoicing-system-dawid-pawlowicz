16cfbd14583671f8a5bec014cd1a69e06972a4aa
Fleskimiso <40237506+Fleskimiso@users.noreply.github.com>
2023-06-16 13:08:14 +0200

    added jacoco and github actions

--- app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
@@ -16,4 +16,11 @@ class AppTest extends Specification {
         then:
         result != null
     }
+    def "dummy test to cover main"() {
+        setup:
+        def app = new App()
+
+        and:
+        app.main()
+    }
 }
