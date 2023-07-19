e82250b22eca03b3cdf46366f96b4bb54c216bd5
Fleskimiso <40237506+Fleskimiso@users.noreply.github.com>
2023-07-19 15:24:03 +0200

    added tax calculation service

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/TaxCalculatorResult.java
@@ -0,0 +1,21 @@
+package pl.futurecollars.invoicing.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class TaxCalculatorResult {
+
+  private final BigDecimal income;
+  private final BigDecimal costs;
+  private final BigDecimal earnings;
+
+  private final BigDecimal incomingVat;
+  private final BigDecimal outgoingVat;
+  private final BigDecimal vatToReturn;
+
+}
