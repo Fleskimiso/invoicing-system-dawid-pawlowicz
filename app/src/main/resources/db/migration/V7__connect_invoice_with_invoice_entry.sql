3e2eaf4709c96347e710ee2706abd7156e2f1b0f
Fleskimiso <40237506+Fleskimiso@users.noreply.github.com>
2023-07-31 18:28:12 +0200

    added database configuration,flyway migration

---
+++ app/src/main/resources/db/migration/V7__connect_invoice_with_invoice_entry.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.invoice_invoice_entry
+(
+    invoice_id       bigint NOT NULL,
+    invoice_entry_id bigint NOT NULL,
+    PRIMARY KEY (invoice_id, invoice_entry_id)
+);
+
+ALTER TABLE public.invoice_invoice_entry
+    ADD CONSTRAINT invoice_id_fk FOREIGN KEY (invoice_id)
+        REFERENCES public.invoice (id);
+
+ALTER TABLE public.invoice_invoice_entry
+    ADD CONSTRAINT invoice_entry_id_fk FOREIGN KEY (invoice_entry_id)
+        REFERENCES public.invoice_entry (id);
