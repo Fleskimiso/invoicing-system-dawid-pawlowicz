dbdc251595a59831f7ef629504b53a84305ea672
Fleskimiso <40237506+Fleskimiso@users.noreply.github.com>
2023-09-03 14:35:55 +0200

    added company service

--- app/src/main/java/pl/futurecollars/invoicing/controller/company/CompanyApi.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/company/CompanyApi.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.futurecollars.invoicing.model.Company;
 
+@CrossOrigin
 @RequestMapping(value = "companies", produces = {"application/json;charset=UTF-8"})
 @Api(tags = {"company-controller"})
 public interface CompanyApi {
